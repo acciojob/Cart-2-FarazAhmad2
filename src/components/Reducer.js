540a80fa24c7e4145b1845b9b41d30581069d0b6
FarazAhmad2 <farazahmad574@gmail.com>
2024-08-15 23:02:55 +0530

    second commit

---
+++ src/components/Reducer.js
@@ -0,0 +1,30 @@
+
+export const initialState={
+    arraydata:[
+        {
+          id: 1,
+          title: "Samsung Galaxy S7",
+          price: 599.99,
+          img: "https://www.pexels.com/photo/samsung-smart-phone-in-hand-16812192/",
+          amount: 1
+        },
+        {
+          id: 2,
+          title: "google pixel ",
+          price: 499.99,
+          img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+          amount: 1
+        },
+        {
+          id: 3,
+          title: "Xiaomi Redmi Note 2",
+          price: 699.99,
+          img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+          amount: 1
+        }
+      ]
+  }
+  export const Reducer=((state,action)=>{
+  
+  });
+  
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { CartProvider } from './context/CartContext';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-    <CartProvider>
-      <App />
-    </CartProvider>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
